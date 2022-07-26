8f834f0e5a8f6c7772d93709d3ffab902518cf71
Wanjoplang <wwansai@gmail.com>
2022-07-26 11:58:27 +0530

    Update add.tsx

--- routes/add.tsx
+++ routes/add.tsx
@@ -2,10 +2,11 @@
 
 /** @jsx h */
 import {h} from "preact";
+import {tw} from "@twind";
 
 export default function Page(){
     return (
-        <main>
+        <main class={tw`mt-10`}>
             This is the ADD page.
             hello
         </main>
