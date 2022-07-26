fda404d7b8ec6c6e4340753309bd297efe73dc62
Wanjoplang <wwansai@gmail.com>
2022-07-26 12:27:46 +0530

    Update fetch.tsx

--- routes/fetch.tsx
+++ routes/fetch.tsx
@@ -54,6 +54,174 @@ export default function Page(){
                         <span>This is the answer</span>
                     </div>
                 </div>
+                <div class={tw`shadow-md p-2 bg-gray-100 border-l-slate-700 border-2 mb-5`}>
+                    <div class={tw`flex justify-end mb-5`}>
+                        <div class={tw`flex items-center mr-2 cursor-pointer`}>
+                            <small class={tw`mr-1 font-semibold text-sky-800`}>Edit</small>
+                            <svg xmlns="http://www.w3.org/2000/svg" class={tw`h-5 w-5 mr-3`} fill="none" viewBox="0 0 24 24" stroke="#075985" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                            </svg>
+                        </div>
+                        <div class={tw`flex items-center cursor-pointer`}>
+                            <small class={tw`mr-1 font-semibold text-rose-800`}>Delete</small>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="#9f1239" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div class={tw`flex items-center mb-2`}>
+                        <small class={tw`mr-2`}><b>Category:</b></small>
+                        <span>Category name</span>
+                    </div>
+                    <div class={tw`grid grid-flow-row mb-2`}>
+                        <small><b>Q No. #</b></small>
+                        <span>What is the question?</span>
+                    </div>
+                    <div class={tw`grid grid-flow-row`}>
+                        <small><b>Answer</b></small>
+                        <span>This is the answer</span>
+                    </div>
+                </div>
+                <div class={tw`shadow-md p-2 bg-gray-100 border-l-slate-700 border-2 mb-5`}>
+                    <div class={tw`flex justify-end mb-5`}>
+                        <div class={tw`flex items-center mr-2 cursor-pointer`}>
+                            <small class={tw`mr-1 font-semibold text-sky-800`}>Edit</small>
+                            <svg xmlns="http://www.w3.org/2000/svg" class={tw`h-5 w-5 mr-3`} fill="none" viewBox="0 0 24 24" stroke="#075985" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                            </svg>
+                        </div>
+                        <div class={tw`flex items-center cursor-pointer`}>
+                            <small class={tw`mr-1 font-semibold text-rose-800`}>Delete</small>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="#9f1239" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div class={tw`flex items-center mb-2`}>
+                        <small class={tw`mr-2`}><b>Category:</b></small>
+                        <span>Category name</span>
+                    </div>
+                    <div class={tw`grid grid-flow-row mb-2`}>
+                        <small><b>Q No. #</b></small>
+                        <span>What is the question?</span>
+                    </div>
+                    <div class={tw`grid grid-flow-row`}>
+                        <small><b>Answer</b></small>
+                        <span>This is the answer</span>
+                    </div>
+                </div>
+                <div class={tw`shadow-md p-2 bg-gray-100 border-l-slate-700 border-2 mb-5`}>
+                    <div class={tw`flex justify-end mb-5`}>
+                        <div class={tw`flex items-center mr-2 cursor-pointer`}>
+                            <small class={tw`mr-1 font-semibold text-sky-800`}>Edit</small>
+                            <svg xmlns="http://www.w3.org/2000/svg" class={tw`h-5 w-5 mr-3`} fill="none" viewBox="0 0 24 24" stroke="#075985" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                            </svg>
+                        </div>
+                        <div class={tw`flex items-center cursor-pointer`}>
+                            <small class={tw`mr-1 font-semibold text-rose-800`}>Delete</small>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="#9f1239" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div class={tw`flex items-center mb-2`}>
+                        <small class={tw`mr-2`}><b>Category:</b></small>
+                        <span>Category name</span>
+                    </div>
+                    <div class={tw`grid grid-flow-row mb-2`}>
+                        <small><b>Q No. #</b></small>
+                        <span>What is the question?</span>
+                    </div>
+                    <div class={tw`grid grid-flow-row`}>
+                        <small><b>Answer</b></small>
+                        <span>This is the answer</span>
+                    </div>
+                </div>
+                <div class={tw`shadow-md p-2 bg-gray-100 border-l-slate-700 border-2 mb-5`}>
+                    <div class={tw`flex justify-end mb-5`}>
+                        <div class={tw`flex items-center mr-2 cursor-pointer`}>
+                            <small class={tw`mr-1 font-semibold text-sky-800`}>Edit</small>
+                            <svg xmlns="http://www.w3.org/2000/svg" class={tw`h-5 w-5 mr-3`} fill="none" viewBox="0 0 24 24" stroke="#075985" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                            </svg>
+                        </div>
+                        <div class={tw`flex items-center cursor-pointer`}>
+                            <small class={tw`mr-1 font-semibold text-rose-800`}>Delete</small>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="#9f1239" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div class={tw`flex items-center mb-2`}>
+                        <small class={tw`mr-2`}><b>Category:</b></small>
+                        <span>Category name</span>
+                    </div>
+                    <div class={tw`grid grid-flow-row mb-2`}>
+                        <small><b>Q No. #</b></small>
+                        <span>What is the question?</span>
+                    </div>
+                    <div class={tw`grid grid-flow-row`}>
+                        <small><b>Answer</b></small>
+                        <span>This is the answer</span>
+                    </div>
+                </div>
+                <div class={tw`shadow-md p-2 bg-gray-100 border-l-slate-700 border-2 mb-5`}>
+                    <div class={tw`flex justify-end mb-5`}>
+                        <div class={tw`flex items-center mr-2 cursor-pointer`}>
+                            <small class={tw`mr-1 font-semibold text-sky-800`}>Edit</small>
+                            <svg xmlns="http://www.w3.org/2000/svg" class={tw`h-5 w-5 mr-3`} fill="none" viewBox="0 0 24 24" stroke="#075985" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                            </svg>
+                        </div>
+                        <div class={tw`flex items-center cursor-pointer`}>
+                            <small class={tw`mr-1 font-semibold text-rose-800`}>Delete</small>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="#9f1239" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div class={tw`flex items-center mb-2`}>
+                        <small class={tw`mr-2`}><b>Category:</b></small>
+                        <span>Category name</span>
+                    </div>
+                    <div class={tw`grid grid-flow-row mb-2`}>
+                        <small><b>Q No. #</b></small>
+                        <span>What is the question?</span>
+                    </div>
+                    <div class={tw`grid grid-flow-row`}>
+                        <small><b>Answer</b></small>
+                        <span>This is the answer</span>
+                    </div>
+                </div>
+                <div class={tw`shadow-md p-2 bg-gray-100 border-l-slate-700 border-2 mb-5`}>
+                    <div class={tw`flex justify-end mb-5`}>
+                        <div class={tw`flex items-center mr-2 cursor-pointer`}>
+                            <small class={tw`mr-1 font-semibold text-sky-800`}>Edit</small>
+                            <svg xmlns="http://www.w3.org/2000/svg" class={tw`h-5 w-5 mr-3`} fill="none" viewBox="0 0 24 24" stroke="#075985" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                            </svg>
+                        </div>
+                        <div class={tw`flex items-center cursor-pointer`}>
+                            <small class={tw`mr-1 font-semibold text-rose-800`}>Delete</small>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="#9f1239" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div class={tw`flex items-center mb-2`}>
+                        <small class={tw`mr-2`}><b>Category:</b></small>
+                        <span>Category name</span>
+                    </div>
+                    <div class={tw`grid grid-flow-row mb-2`}>
+                        <small><b>Q No. #</b></small>
+                        <span>What is the question?</span>
+                    </div>
+                    <div class={tw`grid grid-flow-row`}>
+                        <small><b>Answer</b></small>
+                        <span>This is the answer</span>
+                    </div>
+                </div>
             </div>
         </main>
     );
