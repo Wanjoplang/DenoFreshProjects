0a2d1bebdca6b563a677068c04b724e304849351
Wanjoplang <wwansai@gmail.com>
2022-07-26 11:52:25 +0530

    Create add.tsx

---
+++ routes/add.tsx
@@ -0,0 +1,12 @@
+// routes/add.tsx
+
+/** @jsx h */
+import {h} from "preact";
+
+export default function Page(){
+    return (
+        <main>
+            This is the ADD page.
+        </main>
+    );
+}
